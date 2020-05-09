0a70e6d157d9389f74caab30bf5b58abff3e578e
Rhonalf Martinez <rhonaldomaster@gmail.com>
2020-05-08 23:24:47 -0500

    update dotenv lib

--- system/dotenv/Dotenv.php
+++ system/dotenv/Dotenv.php
@@ -26,7 +26,10 @@ class Dotenv
     public function __construct($path, $file = '.env')
     {
         if ($file == '.env') {
-            $file = '.env.' . strtolower(ENVIRONMENT);
+			$file = '.env.' . strtolower(ENVIRONMENT);
+			if (! is_file( $file )) {
+				$file = '.env';
+			}
         }
         $this->filePath = $this->getFilePath($path, $file);
     }
